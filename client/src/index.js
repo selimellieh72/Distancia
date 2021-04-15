09ad0e38b660593e93256506d8c16a509f7420df
Selim Ellieh <selimplayselim@gmail.com>
2021-04-15 14:31:50 +0300

    Made user popover responsive

--- client/src/index.js
+++ client/src/index.js
@@ -1,26 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import axios from "axios";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./Components/App";
 import { AuthProvider } from "./providers/AuthContext";
 
-import { ChakraProvider } from "@chakra-ui/react";
-
-const url =
-  window.location.origin === "http://localhost:3000"
-    ? "http://localhost:5000"
-    : window.location.origin;
-axios.defaults.baseURL = url;
+import { ChakraProvider } from "@chakra-ui/react"
 
 ReactDOM.render(
   <ChakraProvider>
-    <AuthProvider>
-      <Router>
-        <App />
-      </Router>
-    </AuthProvider>
+  <AuthProvider>
+    <Router>
+      <App />
+    </Router>
+  </AuthProvider>
   </ChakraProvider>,
   document.getElementById("root")
 );
--- client/user/UserPopover.js
+++ client/user/UserPopover.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-
+import { useMediaPredicate } from "react-media-hook";
 import FocusLock from "react-focus-lock";
 import {
   Text,
@@ -11,65 +11,123 @@ import {
   PopoverArrow,
   PopoverCloseButton,
   Box,
-  IconButton,
   Avatar,
   Flex,
   AvatarBadge,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerBody,
+  DrawerFooter,
 } from "@chakra-ui/react";
 
 import Logout from "../auth/Logout";
 import { authContext } from "../../providers/AuthContext";
-import { InfoIcon } from "@chakra-ui/icons";
 import { Link } from "react-router-dom";
 
 const UserPopover = () => {
   const { onOpen, onClose, isOpen } = useDisclosure();
   const { fullName, isTeacher, discipline } = useContext(authContext)[0];
 
+  const biggerThan400 = useMediaPredicate("(min-width: 400px)");
+
   return (
     <div className="header-profile">
       <Box className="header-profile__name" d="inline-block" color="white">
         {fullName}
       </Box>
-      <Popover
-        isOpen={isOpen}
-        onOpen={onOpen}
-        onClose={onClose}
-        placement="right"
-        closeOnBlur={false}
-      >
-        <PopoverTrigger>
+      {biggerThan400 ? (
+        <Popover
+          isOpen={isOpen}
+          onOpen={onOpen}
+          onClose={onClose}
+          placement="right"
+          closeOnBlur={false}
+        >
+          <PopoverTrigger>
+            <Avatar
+              className="header-profile__avatar"
+              ml="1rem"
+              cursor="pointer"
+            />
+          </PopoverTrigger>
+          <PopoverContent className="popover-content" mt={5} p={3}>
+            <FocusLock returnFocus persistentFocus={false}>
+              <PopoverArrow />
+              <PopoverCloseButton />
+
+              <Flex mb="18px">
+                <Avatar mr={5}>
+                  <AvatarBadge boxSize="1.25em" bg="green.500" />
+                </Avatar>
+                <Flex flexDir="column">
+                  <Link to="/stats">
+                    {" "}
+                    <Text flex={1}>{fullName}</Text>
+                  </Link>
+                  <Text as="strong">
+                    {isTeacher ? `Teacher (${discipline})` : "Student"}
+                  </Text>
+                </Flex>
+              </Flex>
+
+              <Center>
+                <Logout />
+              </Center>
+            </FocusLock>
+          </PopoverContent>
+        </Popover>
+      ) : (
+        <>
           <Avatar
+            onClick={onOpen}
             className="header-profile__avatar"
             ml="1rem"
             cursor="pointer"
           />
-        </PopoverTrigger>
-        <PopoverContent mt={5} p={3}>
-          <FocusLock returnFocus persistentFocus={false}>
-            <PopoverArrow />
-            <PopoverCloseButton />
-            <Flex mb="18px">
-              <Avatar mr={5}>
-                <AvatarBadge boxSize="1.25em" bg="green.500" />
-              </Avatar>
-              <Flex flexDir="column">
-                <Link to="/stats">
-                  {" "}
-                  <Text flex={1}>{fullName}</Text>
-                </Link>
-                <Text as="strong">
-                  {isTeacher ? `Teacher (${discipline})` : "Student"}
-                </Text>
-              </Flex>
-            </Flex>
+          <Drawer
+            isOpen={isOpen}
+            placement="right"
+            onClose={onClose}
+            size="full"
+          >
+            <DrawerOverlay>
+              <DrawerContent>
+                <DrawerCloseButton />
+                <DrawerHeader
+                  textAlign="center"
+                  fontSize="2rem"
+                  fontWeight="bold"
+                >
+                  Your profile
+                </DrawerHeader>
 
-            <Center>
-              <Logout />
-            </Center>
-          </FocusLock>
-        </PopoverContent>
-      </Popover>
+                <DrawerBody mt="2.5rem" textAlign="Center">
+                  <Avatar size="2xl">
+                    {" "}
+                    <AvatarBadge boxSize="1.25em" bg="green.500" />
+                  </Avatar>
+                  <div className="user-info">
+                    <p className="user-info__fullname">{fullName}</p>
+                    {isTeacher && (
+                      <p className="user-info__discipline">
+                        Teaching {discipline}
+                      </p>
+                    )}
+                  </div>
+                </DrawerBody>
+                <Center>
+                  <DrawerFooter>
+                    <Logout />
+                  </DrawerFooter>
+                </Center>
+              </DrawerContent>
+            </DrawerOverlay>
+          </Drawer>
+        </>
+      )}
     </div>
   );
 };
