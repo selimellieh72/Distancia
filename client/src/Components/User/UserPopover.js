f4acce4c489a5e3e2c58a8e43a3a2e0f444a36bf
Selim Ellieh <selimplayselim@gmail.com>
2021-04-15 17:09:15 +0300

    Fixed stats

--- client/src/Components/User/UserPopover.js
+++ client/src/Components/User/UserPopover.js
@@ -17,6 +17,8 @@ import {
   AvatarBadge,
 } from "@chakra-ui/react";
 
+import { Link } from "react-router-dom";
+
 import Logout from "../auth/Logout";
 import { authContext } from "../../providers/AuthContext";
 import { InfoIcon } from "@chakra-ui/icons";
@@ -38,7 +40,11 @@ const UserPopover = () => {
         closeOnBlur={false}
       >
         <PopoverTrigger>
-          <Avatar className="header-profile__avatar" ml="1rem" cursor="pointer" />
+          <Avatar
+            className="header-profile__avatar"
+            ml="1rem"
+            cursor="pointer"
+          />
         </PopoverTrigger>
         <PopoverContent mt={5} p={3}>
           <FocusLock returnFocus persistentFocus={false}>
@@ -48,8 +54,11 @@ const UserPopover = () => {
               <Avatar mr={5}>
                 <AvatarBadge boxSize="1.25em" bg="green.500" />
               </Avatar>
+
               <Flex flexDir="column">
-                <Text flex={1}>{fullName}</Text>
+                <Link to="/stats">
+                  <Text flex={1}>{fullName}</Text>
+                </Link>
                 <Text as="strong">
                   {isTeacher ? `Teacher (${discipline})` : "Student"}
                 </Text>
--- client/src/index.js
+++ client/src/index.js
@@ -2,18 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
+import axios from "axios";
+
 import App from "./Components/App";
 import { AuthProvider } from "./providers/AuthContext";
 
-import { ChakraProvider } from "@chakra-ui/react"
+import { ChakraProvider } from "@chakra-ui/react";
+console.log(window.location.origin);
+axios.defaults.baseURL =
+  window.location.origin === "http://localhost:3000"
+    ? "http://localhost:5000"
+    : window.location.origin;
 
 ReactDOM.render(
   <ChakraProvider>
-  <AuthProvider>
-    <Router>
-      <App />
-    </Router>
-  </AuthProvider>
+    <AuthProvider>
+      <Router>
+        <App />
+      </Router>
+    </AuthProvider>
   </ChakraProvider>,
   document.getElementById("root")
 );
